9d6c3807e7d42c75021aa86ea09f048e5563ba27
Tyler Boutwell <tylerboutwell1@gmail.com>
2024-12-04 16:45:10 -0600

    prompt user for location and return temp from api

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import requests
 
-print("Hello")
+api_key = "9d89a6c77c978f39a55223330621ad55"
+
+user_city = input("Please enter city: ")
+user_state = input("Please enter state: ")
+user_country_code = input("Please enter country code: ")
+
+location = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={user_city},"
+                        f"{user_state},{user_country_code}&limit={1}&appid={api_key}")
+
+
+weather_results = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={33}&lon={55}&appid={api_key}&units=imperial')
+print(f"Temperature in {user_city} is {weather_results.json()['main']['temp']}")
+print(weather_results.json())
