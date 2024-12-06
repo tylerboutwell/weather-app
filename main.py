61637b86c1d10b498282af04e778a1e215bd9cab
Tyler Boutwell <tylerboutwell1@gmail.com>
2024-12-05 18:30:30 -0600

    print current temperature and next 5 day temp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-api_key = "Enter Your API Key"
+api_key = "Put your API key here"
 
 #Prompt user for location
 user_city = input("Please enter city: ")
@@ -20,14 +20,17 @@ else:
 #Get weather from weather api
 weather_results = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=imperial')
 if weather_results.status_code == 200:
-    print(f"Temperature in {user_city} is {weather_results.json()['main']['temp']}")
+    print(f"Current temperature in {user_city} is {weather_results.json()['main']['temp']}")
 else:
     print("Error getting weather data")
 
 five_day_forecast = requests.get(f"http://api.openweathermap.org/data/2.5/"
                                  f"forecast?lat={lat}&lon={lon}&appid={api_key}&units=imperial")
 if five_day_forecast.status_code == 200:
+    count = 0
+    print("---Five day forecast---")
     for i in five_day_forecast.json()['list']:
-        print(i)
+        count += 1
+        print(f"{i['dt_txt']}: {i['main']['temp']}")
 else:
     print("Error getting weather data")
